6c36fb03a199c995570edcccaad39821b21a71ce
Rider Jensen <riderjensen@gmail.com>
2019-04-09 10:12:23 -0600

    general nav

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,24 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
+        <router-link to="/">
         <span>WOW</span>
         <span class="font-weight-light">Mount Finder</span>
+        </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>
-          <router-link to="/">Home</router-link>
+        <v-btn flat to="/create">
+         Create
         </v-btn>
-                <v-btn flat>
-          <router-link to="/about">About</router-link>
+        <v-btn flat to="/delete">
+          Delete
         </v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
+        <v-btn flat to="update">
+          Update
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-
     <v-content>
       <v-container style="padding: 0; padding-bottom: 20px" fluid fill-height>
         <v-layout justify-center>
