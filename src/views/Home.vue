59ea77ae3aff73f81de2e614159f948b8f63d641
Rider Jensen <riderjensen@gmail.com>
2019-04-10 09:53:09 -0600

    add gql queries and remove init query

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,17 @@
       <v-btn color="warning" v-on:click="getJumping()">Jumping</v-btn>
       <v-btn color="info" v-on:click="getWater()">Water</v-btn>
     </div>
+    <div>
+      <v-btn color="info" v-on:click="getAllGraphql()">GraphQL All</v-btn>
+      <v-btn color="success" v-on:click="getFlyingGraphql()">GraphQL Flying</v-btn>
+      <v-btn color="error" v-on:click="getGroundGraphql()">GraphQL Ground</v-btn>
+      <v-btn color="warning" v-on:click="getJumpingGraphql()">GraphQLJumping</v-btn>
+      <v-btn color="info" v-on:click="getWaterGraphql()">GraphQL Water</v-btn>
+    </div>
     <v-layout>
       <v-flex xs12 sm10 offset-sm1>
         <v-card>
-                         <v-progress-circular v-if="loading"
-      indeterminate
-      color="primary"
-    ></v-progress-circular>
+          <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
           <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
             <v-layout v-if="!loading" row wrap>
               <v-flex v-for="(item, index) in items" :key="index" xs3>
@@ -39,29 +43,28 @@
 <script>
 import Welcome from "../components/home/Welcome";
 import axios from "axios";
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export default {
-  apollo: {
-    // Simple query that will update the 'hello' vue property
-    items: gql`query {
-      mounts {
-        name,
-        icon,
-        isAquatic,
-        isFlying,
-        isGround,
-        isJumping,
-        spellId
-      }
-    }`,
-  },
+  // apollo: {
+  //   items: gql`query {
+  //     mounts {
+  //       name,
+  //       icon,
+  //       isAquatic,
+  //       isFlying,
+  //       isGround,
+  //       isJumping,
+  //       spellId
+  //     }
+  //   }`,
+  // },
   components: {
     Welcome
   },
   data() {
     return {
-      error: '',
+      error: "",
       items: [],
       loading: true
     };
@@ -83,7 +86,7 @@ export default {
           this.items = resp.data;
           this.loading = false;
         })
-        .catch(err => this.error = err);
+        .catch(err => (this.error = err));
     },
     getFlying: function() {
       this.loading = true;
@@ -93,7 +96,7 @@ export default {
           this.items = resp.data;
           this.loading = false;
         })
-        .catch(err => this.error = err);
+        .catch(err => (this.error = err));
     },
     getGround: function() {
       this.loading = true;
@@ -103,7 +106,7 @@ export default {
           this.items = resp.data;
           this.loading = false;
         })
-        .catch(err => this.error = err);
+        .catch(err => (this.error = err));
     },
     getJumping: function() {
       this.loading = true;
@@ -113,7 +116,7 @@ export default {
           this.items = resp.data;
           this.loading = false;
         })
-        .catch(err => this.error = err);
+        .catch(err => (this.error = err));
     },
     getWater: function() {
       this.loading = true;
@@ -123,8 +126,106 @@ export default {
           this.items = resp.data;
           this.loading = false;
         })
-        .catch(err => this.error = err);
-    }
+        .catch(err => (this.error = err));
+    },
+    getAllGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query {
+              mounts {
+                name
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    },
+    getFlyingGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query {
+              mounts (where: {
+                isFlying: true
+              }){
+                name
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    },
+    getGroundGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query {
+              mounts (where: {
+                isGround: true
+              }){
+                name
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    },
+    getJumpingGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query {
+              mounts (where: {
+                isJumping: true
+              }){
+                name
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    },
+    getWaterGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query {
+              mounts (where: {
+                isAquatic: true
+              }){
+                name
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    },
   }
 };
 </script>
