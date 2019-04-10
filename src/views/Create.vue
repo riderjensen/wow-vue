6766ce395df95200b488084d42a1d68c9d1fa424
rider <Riderjensen@gmail.com>
2019-04-09 18:24:27 -0600

    Create is working

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,72 @@
 <template>
   <div class="about">
-    <h1>This is create page</h1>
+    <v-form v-model="valid">
+      <v-container>
+        {{error}}
+        <v-flex xs12>
+          <v-text-field v-model="item.name" :rules="nameRules" label="Name" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="item.icon" :rules="nameRules" label="Icon" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="item.spellId" label="Spell ID" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="item.creatureId" label="Creature ID" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="item.itemId" label="Item ID" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="item.qualityId" label="Quality ID" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-checkbox v-model="item.isGround" label="Ground mount" required></v-checkbox>
+        </v-flex>
+        <v-flex xs12>
+          <v-checkbox v-model="item.isFlying" label="Flying mount" required></v-checkbox>
+        </v-flex>
+        <v-flex xs12>
+          <v-checkbox v-model="item.isAquatic" label="Water mount" required></v-checkbox>
+        </v-flex>
+        <v-flex xs12>
+          <v-checkbox v-model="item.isJumping" label="Jumping mount" required></v-checkbox>
+        </v-flex>
+      </v-container>
+      <v-btn @click="submitCreate()">Submit</v-btn>
+    </v-form>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    error: '',
+    valid: false,
+    item: {
+      name: "",
+      icon: "",
+      spellId: 0,
+      creatureId: 0,
+      itemId: 0,
+      qualityId: 0,
+      isGround: false,
+      isFlying: false,
+      isAquatic: false,
+      isJumping: false
+    },
+    nameRules: [v => !!v || "Name is required"],
+    // numberRules: [v => v.isNumber() || "Must be a number"]
+  }),
+  methods: {
+    submitCreate: function() {
+      axios.post('https://mighty-lake-67625.herokuapp.com/edit', this.item).then(resp => {
+        console.log(resp)
+      }).catch(err => this.error = err)
+    }
+  }
+};
+</script>
